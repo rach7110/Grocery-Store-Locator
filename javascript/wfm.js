4911bf24ab323b7ee83964978380b1d94e900878
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-04-08 11:45:58 -0500

    added styling to instructions div

--- javascript/wfm.js
+++ javascript/wfm.js
@@ -2,25 +2,28 @@
 //   alert("Works!");
 // });
 $(document).ready(function() {
-	// SIDEBAR: TOGGLES TO SHOW/HIDE SUB NAVIGATION ITEMS WHEN CATEGORY IS CLICKED.
-	$('#category01 p').click(function() {
-			$('#category01 .sub-nav').toggleClass('visible');
-	});
-	$('#category02 p').click(function() {
-			$('#category02 .sub-nav').toggleClass('visible');
-	});
-	$('#category03 p').click(function() {
-			$('#category03 .sub-nav').toggleClass('visible');
-	});
-	$('#category04 p').click(function() {
-			$('#category04 .sub-nav').toggleClass('visible');
-	});
-	$('#category05 p').click(function() {
-			$('#category05 .sub-nav').toggleClass('visible');
-	});
-	// ***************************************************
-	// APPENDS STORE ADDRESS TO WEBPAGE WHEN LOCATION IS SELECTED FROM DROPDOWN MENU.
-	$('form').on('change', 'select', function(){
+// *******************************************************************************
+// SIDEBAR: TOGGLES TO SHOW/HIDE SUB NAVIGATION ITEMS WHEN CATEGORY IS CLICKED.
+// *******************************************************************************
+  $('#category01 p').click(function() {
+      ('#category01 .sub-nav').toggleClass('visible');
+  });
+  $('#category02 p').click(function() {
+      $('#category02 .sub-nav').toggleClass('visible');
+  });
+  $('#category03 p').click(function() {
+      $('#category03 .sub-nav').toggleClass('visible');
+  });
+  $('#category04 p').click(function() {
+      $('#category04 .sub-nav').toggleClass('visible');
+  });
+  $('#category05 p').click(function() {
+      $('#category05 .sub-nav').toggleClass('visible');
+  });
+// *******************************************************************************
+// APPENDS STORE ADDRESS TO WEBPAGE WHEN LOCATION IS SELECTED FROM DROPDOWN MENU.
+// *******************************************************************************
+  $('form').on('change', 'select', function(){
     $('#address').empty();
     var arrayAddress = [];
     var address01 = "";
@@ -34,7 +37,6 @@ $(document).ready(function() {
       arr04 = arr[3];
       arr05 = arr[4];
       name = $("select option:selected").text();
-
       $('#address').append('<p><strong>' + name + '</strong></p>')
       var i = 0
       while ((i < arr.length) && (arr[i].length > 0)){ //2nd validation confirms the string is not empty.
@@ -44,4 +46,3 @@ $(document).ready(function() {
     });
   });
 });
-
