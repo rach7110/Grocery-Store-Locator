c7ce0a9aeec2c3774087c07092012a4a86b6da92
Rachel Loziuk <rachel.loziuk@gmail.com>
2014-04-08 21:38:46 -0500

    fixed broken js - missing $

--- javascript/wfm.js
+++ javascript/wfm.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
 // SIDEBAR: TOGGLES TO SHOW/HIDE SUB NAVIGATION ITEMS WHEN CATEGORY IS CLICKED.
 // *******************************************************************************
   $('#category01 p').click(function() {
-      ('#category01 .sub-nav').toggleClass('visible');
+    $('#category01 .sub-nav').toggleClass('visible');
   });
   $('#category02 p').click(function() {
-      $('#category02 .sub-nav').toggleClass('visible');
+    $('#category02 .sub-nav').toggleClass('visible');
   });
   $('#category03 p').click(function() {
-      $('#category03 .sub-nav').toggleClass('visible');
+    $('#category03 .sub-nav').toggleClass('visible');
   });
   $('#category04 p').click(function() {
-      $('#category04 .sub-nav').toggleClass('visible');
+    $('#category04 .sub-nav').toggleClass('visible');
   });
   $('#category05 p').click(function() {
-      $('#category05 .sub-nav').toggleClass('visible');
+    $('#category05 .sub-nav').toggleClass('visible');
   });
 // *******************************************************************************
 // APPENDS STORE ADDRESS TO WEBPAGE WHEN LOCATION IS SELECTED FROM DROPDOWN MENU.
